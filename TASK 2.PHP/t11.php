1982ca2796b37fc1e0a63d3a22188f3af6376aa4
Vishakha2208 <143685886+Vishakha2208@users.noreply.github.com>
2023-10-04 21:23:47 +0530

    Add files via upload

---
+++ TASK 2.PHP/t11.php
@@ -0,0 +1,11 @@
+<?php
+$i=22;
+if ($i%2 == 0)
+{
+echo "this number is even";
+}
+else
+{
+echo"this number is odd";
+}
+?>
---
+++ TASK 2.PHP/t13.php
@@ -0,0 +1,5 @@
+<?php
+$celcius=10;
+$fahrenheit=($celcius*9/5)+32;
+echo "$celcius to $fahrenheit";
+?>
